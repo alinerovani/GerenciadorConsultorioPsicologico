8495632786ef1debb48b8211e58716e1ce54f815
alinilopes <lopesalini@gmail.com>
2020-08-14 06:07:57 -0300

    inserir um registro default na tabela de estado e na tabela de cidade ao se conectar com o banco

--- ps.api/src/index.ts
+++ ps.api/src/index.ts
@@ -1,12 +1,14 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
+import { createConnection, Repository, getRepository } from "typeorm";
 import * as express from "express";
 import * as cors from 'cors';
 import * as bodyParser from "body-parser";
-import {Request, Response} from "express";
-import {Routes} from "./routes";
+import { Request, Response } from "express";
+import { Routes } from "./routes";
 import config from "./include/config";
 import auth from './middleware/auth';
+import State from "./entity/State";
+import City from "./entity/City";
 
 // create express app
 const app = express();
@@ -35,8 +37,43 @@ app.listen(config.port, '0.0.0.0', async () => {
     console.log("API FUNCIONANDO");
     try {
         await createConnection();
+
+        //Verifica se o estado do RS ainda não está cadastrado
+        //Se não estiver, insere o registro no banco
+        let stateRepository = getRepository(State);
+        let stateRS = await stateRepository.findOne({ name: "Rio Grande do Sul" });
+        var estadoUid = "";
+
+        if (stateRS) {
+            estadoUid = stateRS.uid;
+        } else {
+            let estado = new State();
+            estado.name = "Rio Grande do Sul";
+            estado.abbreviation = "RS";
+
+            await stateRepository
+                .save(estado)
+                .then(estado => {
+                    estadoUid = estado.uid;
+                });
+        }
+
+        //Verifica se a cidade de Passo Fundo ainda não está cadastrada
+        //Se não estiver, insere o registro no banco
+        let cityRepository = getRepository(City);
+        let cityPF = await cityRepository.findOne({ name: "Passo Fundo" });
+
+        if (!cityPF) {
+            let cidade = new City();
+            cidade.name = "Passo Fundo";
+            cidade.stateUid = estadoUid;
+
+            await cityRepository
+                .save(cidade);
+        };
+
         console.log("CONECTOU COM O BANCO DE DADOS");
-    } catch(error) {
+    } catch (error) {
         console.error(`Erro conectar banco de dados ${error}`)
     }
 });
